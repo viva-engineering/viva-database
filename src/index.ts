3b81ae12c314c8723ec937115046f0d4ac20234e
James Brumond <james@jbrumond.me>
2019-04-04 22:44:08 -0700

    some cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 
 import { DatabasePool } from '@viva-eng/database';
+import { credentialsTable } from './tables/credentials';
+import { friendsTable } from './tables/friends';
+import { sessionsTable } from './tables/sessions';
+import { usersTable } from './tables/users';
 
 export { SelectQuery, WriteQuery } from './queries';
-export { credentialsTable } from './tables/credentials';
-export { friendsTable } from './tables/friends';
-export { sessionsTable } from './tables/sessions';
-export { usersTable } from './tables/users';
+
+export const tables = {
+	credentials: credentialsTable,
+	friends: friendsTable,
+	sessions: sessionsTable,
+	users: usersTable
+};
 
 export let db: DatabasePool;
 
